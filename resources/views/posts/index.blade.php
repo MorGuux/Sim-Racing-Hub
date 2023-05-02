ee8de856bcc0ecffed2ef0f999a8377b186bdc32
MorGuux <morgan.gardner2112@gmail.com>
2023-05-02 23:35:16 +0100

    Modify global title

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,10 +3,16 @@
 @section('title', 'Posts')
 
 @section('content')
-    <p>Posts</p>
+    <!-- List of posts -->
     <ul>
         @foreach ($posts as $post)
-            <li><a href="{{ route('posts.show', ['id' => $post->id]) }}">{{ $post->title }}</li>
+            <li>
+                <div style="padding: 5px">
+                    <a href="{{ route('posts.show', ['id' => $post->id]) }}">{{ $post->title }}</a>
+                    <a href="{{ route('users.show', ['id' => $post->user->id]) }}">by {{ $post->user->name }}</a>
+                    <a>{{ $post->created_at->diffForHumans() }}</a>
+                </div>
+            </li>
         @endforeach
     </ul>
     <a href="{{ route('posts.create') }}">Create Post</a>

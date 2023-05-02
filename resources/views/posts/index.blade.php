8608f190a830d0ac7154a85fd5e601758d2f9582
MorGuux <morgan.gardner2112@gmail.com>
2023-05-02 04:48:28 +0100

    Added named route to posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,7 +6,7 @@
     <p>Posts</p>
     <ul>
         @foreach ($posts as $post)
-            <li><a href="/posts/{{ $post->id }}">{{ $post->title }}</li>
+            <li><a href="{{ route('posts.show', ['id' => $post->id]) }}">{{ $post->title }}</li>
         @endforeach
     </ul>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
 
 Route::get('/posts', [PostController::class, 'index']);
 
-Route::get('/posts/{id}', [PostController::class, 'show']);
+Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
 
 Route::get('/dashboard', function () {
     return view('dashboard');

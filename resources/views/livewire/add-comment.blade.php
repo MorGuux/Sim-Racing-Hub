307472e32ebae30b6e140ce8728fe451974979b9
MorGuux <morgan.gardner2112@gmail.com>
2023-05-15 01:02:01 +0100

    Re-designed "add-comment" UI

--- resources/views/livewire/add-comment.blade.php
+++ resources/views/livewire/add-comment.blade.php
@@ -1,7 +1,15 @@
 <div>
-    <form method="POST" action="{{ route('comments.store', ['id' => $post->id]) }}">
-        @csrf
-        <input type="hidden" name="post_id" value="{{ $post->id }}">
-        <p>Body: <textarea name="body" value="{{ old('body') }}"></textarea></p>
-        <input type="submit" value="Add Comment">
+    <div style="display: table; width: 100%; align-items: center; padding: 20px; margin-top: 20px; border: 1px solid #ccc;">
+        <div style="display: table-cell; vertical-align: middle">
+            <div style="display: inline-table; width: 100%">
+                <form id="add-comment-form" method="POST" style="width: 100%" action="{{ route('comments.store', ['id' => $post->id]) }}">
+                    @csrf
+                    <input type="hidden" name="post_id" value="{{ $post->id }}">
+                    <textarea name="body" style="width: 100%">{{ old('body') }}</textarea>
+            </div>
+            <div style="float: right; margin: 10px">
+                <button type="submit" form="add-comment-form">Add Comment</button>
+            </div>
+        </div>
+    </div>
 </div>
